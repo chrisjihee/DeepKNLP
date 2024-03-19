d0c454d63deadb14c94f4f868e855c1382df03b5
Jihee Ryu <chrisjihee@etri.re.kr>
2024-03-19 23:12:48 +0900

    Update setup-env.sh, TODO.txt

--- setup-env.sh
+++ setup-env.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
-# DeepKNLP
+# conda
+wget "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
+bash Miniforge3-$(uname)-$(uname -m).sh
+
+# project
 mamba create -n DeepKNLP-24.03 python=3.11 -y
 mamba activate DeepKNLP-24.03
 pip install -r requirements.txt
-pip list
+pip list | grep -E "torch|lightning|transformers|chris"
 
-# chrisbase (for public user)
+# chrisbase
 rm -rf chrisbase*
 pip download --no-binary :all: --no-deps chrisbase==0.5.1; tar zxf chrisbase-*.tar.gz; rm chrisbase-*.tar.gz;
+# git clone https://github.com/chrisjihee/chrisbase.git --branch v0.5.1 --single-branch
 pip install --editable chrisbase*
 
-# chrisbase (for previleged user)
-rm -rf chrisbase*
-git clone https://github.com/chrisjihee/chrisbase.git
-pip install --editable chrisbase*
-
-# pretrained LM (for previleged user)
-rm -rf pretrained*
-git clone guest@129.254.164.137:git/pretrained
-
-# pretrained LM (for git lfs installed environment)
+# pretrained
 rm -rf pretrained*
 git lfs install
-git clone https://github.com/KPFBERT/kpfbert pretrained/KPF-BERT
-git clone https://huggingface.co/klue/roberta-base pretrained/KLUE-RoBERTa
-git lfs uninstall
+git clone https://huggingface.co/jinmang2/kpfbert
+git clone https://huggingface.co/klue/roberta-base
+git clone https://huggingface.co/klue/roberta-small
+git clone https://huggingface.co/klue/roberta-large
+git clone https://huggingface.co/monologg/koelectra-base-v3-discriminator
+git clone https://huggingface.co/monologg/koelectra-small-v3-discriminator
