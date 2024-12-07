4682f4af9fc730c128bdd47281972fa17b4cdd96
Jihee Ryu <chrisjihee@etri.re.kr>
2024-12-08 03:24:25 +0900

    self._display_measurement = Measurement(self.initial, 0)

--- task2-nerG.py
+++ task2-nerG.py
@@ -99,7 +99,7 @@ def train(
         test_data: Annotated[str, typer.Option("--test_data")] = None,  # TODO: "data/gner/zero-shot-test.jsonl"
         max_source_length: Annotated[int, typer.Option("--max_source_length")] = 512,
         max_target_length: Annotated[int, typer.Option("--max_target_length")] = 512,
-        max_train_samples: Annotated[int, typer.Option("--max_train_samples")] = 1000,  # TODO: -1
+        max_train_samples: Annotated[int, typer.Option("--max_train_samples")] = 10,  # TODO: -1
         max_eval_samples: Annotated[int, typer.Option("--max_eval_samples")] = -1,
         max_test_samples: Annotated[int, typer.Option("--max_test_samples")] = -1,
         num_prog_samples: Annotated[int, typer.Option("--num_prog_samples")] = 5000,
@@ -259,13 +259,6 @@ def train(
                 if args.input.max_train_samples > 0:
                     train_dataset = train_dataset.select(range(min(len(train_dataset), args.input.max_train_samples)))
                 fabric.print(f"Use {args.input.train_path} as train dataset: {len(train_dataset):,} samples")
-
-                # TODO: Remove after testing
-                prog = ProgIter(train_dataset, total=len(train_dataset), desc='Preprocess train samples:', verbose=2, stream=fabric)
-                for _ in prog:
-                    prog.format_message_parts()
-                    time.sleep(0.01)
-
         if args.input.eval_path:
             with fabric.rank_zero_first():
                 eval_dataset: Dataset = load_dataset("json", split=datasets.Split.TRAIN,
@@ -417,22 +410,33 @@ def train(
         # Define progress update function
         def update_progress(res: BatchEncoding, rank: int, counter: Counter, pbar: ProgIter):
             pre, cnt = counter.val(), counter.inc()
-            if (cnt >= pbar.total or any(i % num_prog_samples == 0 for i in range(pre + 1, cnt + 1))) and rank == 0:
-                pbar.step(inc=min(cnt - pbar._iter_idx, pbar.total - pbar._iter_idx))
+            # if (cnt >= pbar.total or any(i % num_prog_samples == 0 for i in range(pre + 1, cnt + 1))) and rank == 0:
+            if rank == 0:
+                # fabric.print("rank=%d, pre=%d, cnt=%d" % (rank, pre, cnt))
+                pbar.step(inc=min(cnt - pbar._iter_idx, pbar.total - pbar._iter_idx), force=cnt >= pbar.total)
+                #pbar.display_message()
                 # fabric.print(pbar.format_message().rstrip())
             return res
 
-        # # Preprocess dataset
-        # if train_dataset:
-        #     with fabric.rank_zero_first():
-        #         with ProgIter(total=len(train_dataset), desc='Preprocess train samples: ', verbose=3,
-        #                       stream=fabric, freq=1, adjust=False, rel_adjust_limit=0.0) as manual_pbar:
-        #             train_dataset = train_dataset.map(
-        #                 preprocess_for_decoder_only_model if using_decoder_only_model else preprocess_for_encoder_decoder_model,
-        #                 fn_kwargs={"data_opt": args.input.model_dump(), "update": lambda *xs: update_progress(*xs, pbar=manual_pbar)},
-        #                 with_rank=True, batched=False, num_proc=args.env.max_workers, load_from_cache_file=args.input.use_cache_data,
-        #                 cache_file_name=str(args.input.cache_train_path(len(train_dataset))) if args.input.use_cache_data else None,
-        #             )
+        # Preprocess dataset
+        if train_dataset:
+            with fabric.rank_zero_first():
+                #
+                # # TODO: Remove after testing
+                # prog = ProgIter(train_dataset, total=len(train_dataset), desc='Preprocess train samples:', verbose=2, stream=fabric)
+                # for _ in prog:
+                #     time.sleep(0.01)
+                #
+                manual_pbar = ProgIter(total=len(train_dataset), desc='Preprocess train samples: ', stream=fabric, verbose=2, time_thresh=0.01, freq=1)
+                manual_pbar.begin()
+                # with ProgIter(total=len(train_dataset), desc='Preprocess train samples: ', verbose=2, stream=fabric) as manual_pbar:
+                train_dataset = train_dataset.map(
+                    preprocess_for_decoder_only_model if using_decoder_only_model else preprocess_for_encoder_decoder_model,
+                    fn_kwargs={"data_opt": args.input.model_dump(), "update": lambda *xs: update_progress(*xs, pbar=manual_pbar)},
+                    with_rank=True, batched=False, num_proc=args.env.max_workers, load_from_cache_file=args.input.use_cache_data,
+                    cache_file_name=str(args.input.cache_train_path(len(train_dataset))) if args.input.use_cache_data else None,
+                )
+                manual_pbar.end()
         # if eval_dataset:
         #     with fabric.rank_zero_first():
         #         with ProgIter(total=len(eval_dataset), desc='Preprocess eval samples', verbose=0) as manual_pbar:
