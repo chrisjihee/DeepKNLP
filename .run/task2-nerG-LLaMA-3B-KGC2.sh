36c97a253efef26f0124f9e0e1b468c38ece0533
Jihee Ryu <chrisjihee@etri.re.kr>
2024-12-17 23:59:37 +0900

    Add task2-nerG-LLaMA-3B-*.sh

---
+++ .run/task2-nerG-LLaMA-3B-KGC2.sh
@@ -0,0 +1,2 @@
+export CUDA_VISIBLE_DEVICES=4,5,6,7
+python task2-nerG.py train --run_version LLaMA-3B-KGC2 --study_file data/gner/KG-generation-YAGO3-53220@2.jsonl
---
+++ .run/task2-nerG-LLaMA-3B-base.sh
@@ -0,0 +1,2 @@
+export CUDA_VISIBLE_DEVICES=4,5,6,7
+python task2-nerG.py train --run_version LLaMA-3B-base
