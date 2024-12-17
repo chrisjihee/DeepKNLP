0f4a2fb19e9b96bf1ee98293a3069516292a0ac4
Jihee Ryu <chrisjihee@etri.re.kr>
2024-12-17 22:37:04 +0900

    Ready pretrained = "meta-llama/Llama-2-7b-hf"

--- task2-nerG.py
+++ task2-nerG.py
@@ -156,6 +156,7 @@ def train(
         # pretrained: Annotated[str, typer.Option("--pretrained")] = "etri-lirs/eagle-3b-preview",  # RuntimeError: CUDA error: device-side assert triggered
         # pretrained: Annotated[str, typer.Option("--pretrained")] = "etri-lirs/egpt-1.3b-preview",  # RuntimeError: CUDA error: device-side assert triggered
         # pretrained: Annotated[str, typer.Option("--pretrained")] = "meta-llama/Llama-3.1-8B",
+        # pretrained: Annotated[str, typer.Option("--pretrained")] = "meta-llama/Llama-2-7b-hf",
         pretrained: Annotated[str, typer.Option("--pretrained")] = "meta-llama/Llama-3.2-3B",
         # pretrained: Annotated[str, typer.Option("--pretrained")] = "meta-llama/Llama-3.2-1B",  # TODO: "google/flan-t5-small"
         train_file: Annotated[str, typer.Option("--train_file")] = "data/gner/pile-ner.jsonl",
