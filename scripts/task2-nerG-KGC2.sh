dcc7f060f88fdca16d90f5f6089e89d9d5c4a5ea
Jihee Ryu <chrisjihee@etri.re.kr>
2024-12-18 15:22:28 +0900

    Save scripts for FlanT5-3B

--- scripts/task2-nerG-KGC2.sh
+++ scripts/task2-nerG-KGC2.sh
@@ -1,3 +1,4 @@
 cd ~/proj/DeepKNLP || exit
 export CUDA_VISIBLE_DEVICES=4,5,6,7
-python task2-nerG.py train --run_version LLaMA-3B-KGC2 --pretrained meta-llama/Llama-3.2-3B --study_file data/gner/KG-generation-YAGO3-53220@2.jsonl
+#python task2-nerG.py train --run_version LLaMA-3B-KGC2 --pretrained meta-llama/Llama-3.2-3B --study_file data/gner/KG-generation-YAGO3-53220@2.jsonl
+python task2-nerG.py train --run_version FlanT5-3B-KGC2 --pretrained google/flan-t5-xl --study_file data/gner/KG-generation-YAGO3-53220@2.jsonl
--- scripts/task2-nerG-base.sh
+++ scripts/task2-nerG-base.sh
@@ -1,3 +1,6 @@
 cd ~/proj/DeepKNLP || exit
 export CUDA_VISIBLE_DEVICES=4,5,6,7
-python task2-nerG.py train --run_version LLaMA-3B-base --pretrained meta-llama/Llama-3.2-3B
+#python task2-nerG.py train --run_version EAGLE-1B-base --pretrained etri-lirs/egpt-1.3b-preview
+#python task2-nerG.py train --run_version EAGLE-3B-base --pretrained etri-lirs/eagle-3b-preview
+#python task2-nerG.py train --run_version LLaMA-3B-base --pretrained meta-llama/Llama-3.2-3B
+python task2-nerG.py train --run_version FlanT5-3B-base --pretrained google/flan-t5-xl
