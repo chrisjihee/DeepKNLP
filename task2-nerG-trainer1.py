d5cb55115061d679d8b7d6af787a45d83e0a0814
Jihee Ryu <chrisjihee@etri.re.kr>
2025-01-08 18:13:13 +0900

    Training

--- task2-nerG-trainer1.py
+++ task2-nerG-trainer1.py
@@ -429,6 +429,60 @@ def main():
                 desc="Running tokenizer on prediction dataset",
             )
 
+    # Construct Data collator
+    label_pad_token_id = -100 if data_args.ignore_pad_token_for_loss else tokenizer.pad_token_id
+    data_collator = DataCollatorForGNER(
+        tokenizer,
+        model=model,
+        padding=True,
+        pad_to_multiple_of=8 if training_args.fp16 else None,
+        label_pad_token_id=label_pad_token_id,
+        return_tensors="pt",
+    )
+
+    # we don't want to remove unused columns because we will prepare each batch during training,
+    # and some of the information will also be used in evaluation.
+    training_args.remove_unused_columns = False
+
+    def compute_ner_metrics(dataset, preds, save_prefix=None, save_suffix=None):
+        preds = np.where(preds != -100, preds, tokenizer.pad_token_id)
+        decoded_preds = tokenizer.batch_decode(preds, skip_special_tokens=True)
+        if not is_encoder_decoder:
+            match_pattern = "[/INST]"
+            for i, preds in enumerate(decoded_preds):
+                decoded_preds[i] = preds[preds.find(match_pattern) + len(match_pattern):].strip()
+
+        all_examples = [example.copy() for example in dataset]
+        for idx, decoded_pred in enumerate(decoded_preds):
+            all_examples[idx]["prediction"] = decoded_pred
+
+        results = compute_metrics(all_examples, tokenizer=tokenizer)
+        if save_prefix is not None:
+            with open(os.path.join(training_args.output_dir, f"{save_prefix}_text_generations{'_' + save_suffix if save_suffix else ''}.jsonl"), "w") as fout:
+                for example in all_examples:
+                    fout.write(json.dumps(example) + "\n")
+        return results
+
+    # Initialize our Trainer
+    trainer = GNERTrainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset if training_args.do_train else None,
+        eval_dataset=eval_dataset if training_args.do_eval else None,
+        processing_class=tokenizer,  # FutureWarning: `tokenizer` is deprecated and will be removed in version 5.0.0 for `GNERTrainer.__init__`. Use `processing_class` instead.
+        data_collator=data_collator,
+        compute_metrics=compute_ner_metrics if training_args.predict_with_generate else None,
+    )
+
+    all_metrics = {"run_name": training_args.run_name}
+    # Training
+    if training_args.do_train:
+        checkpoint = None
+        if training_args.resume_from_checkpoint is not None:
+            checkpoint = training_args.resume_from_checkpoint
+
+        train_result = trainer.train(resume_from_checkpoint=checkpoint)
+
 
 if __name__ == "__main__":
     main()
