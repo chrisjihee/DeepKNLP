9f2d81633ca1266bbab9d6a40320f4c42b1c0a02
Jihee Ryu <chrisjihee@etri.re.kr>
2025-01-21 02:47:15 +0900

    # Load pretrained model and tokenizer

--- task2-nerG-trainer2.py
+++ task2-nerG-trainer2.py
@@ -195,42 +195,41 @@ def train(
             args=args,
     ):
         accelerator.wait_for_everyone()
-        logger.warning(f"INSIDE of JobTimer: local_rank={args.env.local_rank}, process_index={accelerator.process_index}, env.time_stamp={env.time_stamp}")
 
-    # # Load pretrained model and tokenizer
-    # config = AutoConfig.from_pretrained(
-    #     args.model_name_or_path,
-    #     trust_remote_code=True,
-    # )
-    # is_encoder_decoder = config.is_encoder_decoder
-    # if is_encoder_decoder:
-    #     tokenizer = AutoTokenizer.from_pretrained(
-    #         args.model_name_or_path,
-    #         trust_remote_code=True,
-    #     )
-    # else:  # https://github.com/Vision-CAIR/MiniGPT-4/issues/129
-    #     tokenizer = AutoTokenizer.from_pretrained(
-    #         args.model_name_or_path,
-    #         trust_remote_code=True,
-    #         add_eos_token=True,
-    #         add_bos_token=True,
-    #         padding_side="left",
-    #     )
-    # if tokenizer.pad_token is None:
-    #     # tokenizer.pad_token = tokenizer.eos_token  # https://medium.com/@rschaeffer23/how-to-fine-tune-llama-3-1-8b-instruct-bf0a84af7795
-    #     tokenizer.pad_token = tokenizer.unk_token if tokenizer.unk_token else tokenizer.eos_token  # https://stackoverflow.com/questions/70544129/transformers-asking-to-pad-but-the-tokenizer-does-not-have-a-padding-token
-    #     # tokenizer.add_special_tokens({'pad_token': "<pad>"})  # https://stackoverflow.com/questions/70544129/transformers-asking-to-pad-but-the-tokenizer-does-not-have-a-padding-token
-    # MODEL_CLASS = AutoModelForSeq2SeqLM if is_encoder_decoder else AutoModelForCausalLM
-    # model = MODEL_CLASS.from_pretrained(
-    #     args.model_name_or_path,
-    #     from_tf=bool(".ckpt" in args.model_name_or_path),
-    #     config=config,
-    #     trust_remote_code=True,
-    # )
-    # logger.info(f"type(model)={type(model)}")
-    # model.generation_config.pad_token_id = tokenizer.pad_token_id  # https://stackoverflow.com/questions/69609401/suppress-huggingface-logging-warning-setting-pad-token-id-to-eos-token-id
-    # logger.info(f"model.generation_config.pad_token_id={model.generation_config.pad_token_id}")
-    #
+        # Load pretrained model and tokenizer
+        config = AutoConfig.from_pretrained(
+            args.data.pretrained,
+            trust_remote_code=True,
+        )
+        is_encoder_decoder = config.is_encoder_decoder
+        if is_encoder_decoder:
+            tokenizer = AutoTokenizer.from_pretrained(
+                args.data.pretrained,
+                trust_remote_code=True,
+            )
+        else:  # https://github.com/Vision-CAIR/MiniGPT-4/issues/129
+            tokenizer = AutoTokenizer.from_pretrained(
+                args.data.pretrained,
+                trust_remote_code=True,
+                add_eos_token=True,
+                add_bos_token=True,
+                padding_side="left",
+            )
+        if tokenizer.pad_token is None:
+            # tokenizer.pad_token = tokenizer.eos_token  # https://medium.com/@rschaeffer23/how-to-fine-tune-llama-3-1-8b-instruct-bf0a84af7795
+            tokenizer.pad_token = tokenizer.unk_token if tokenizer.unk_token else tokenizer.eos_token  # https://stackoverflow.com/questions/70544129/transformers-asking-to-pad-but-the-tokenizer-does-not-have-a-padding-token
+            # tokenizer.add_special_tokens({'pad_token': "<pad>"})  # https://stackoverflow.com/questions/70544129/transformers-asking-to-pad-but-the-tokenizer-does-not-have-a-padding-token
+        MODEL_CLASS = AutoModelForSeq2SeqLM if is_encoder_decoder else AutoModelForCausalLM
+        model = MODEL_CLASS.from_pretrained(
+            args.data.pretrained,
+            from_tf=bool(".ckpt" in str(args.data.pretrained)),
+            config=config,
+            trust_remote_code=True,
+        )
+        logger.info(f"type(model)={type(model)}")
+        model.generation_config.pad_token_id = tokenizer.pad_token_id  # https://stackoverflow.com/questions/69609401/suppress-huggingface-logging-warning-setting-pad-token-id-to-eos-token-id
+        logger.info(f"model.generation_config.pad_token_id={model.generation_config.pad_token_id}")
+
     # # Preprocess the datasets
     # def preprocess_function(example):
     #     # remove pairs where at least one record is None
