35355b19ea2c6b32c1be2f32f5b0f616d3d27f32
Jihee Ryu <chrisjihee@etri.re.kr>
2025-01-21 21:25:09 +0900

    Add task2-nerG-trainer2b.sh

---
+++ task2-nerG-trainer2b.sh
@@ -0,0 +1,8 @@
+set -x
+~/miniforge3/envs/DeepKNLP/bin/python -m deepspeed.launcher.runner --include=localhost:4,5,6,7 --master_port 30000 task2-nerG-trainer2.py --trainer_deepspeed configs/deepspeed/ds1_llama.json
+~/miniforge3/envs/DeepKNLP/bin/python -m deepspeed.launcher.runner --include=localhost:4,5,6,7 --master_port 30000 task2-nerG-trainer2.py --trainer_deepspeed configs/deepspeed/ds2_llama.json
+~/miniforge3/envs/DeepKNLP/bin/python -m deepspeed.launcher.runner --include=localhost:4,5,6,7 --master_port 30000 task2-nerG-trainer2.py --trainer_deepspeed configs/deepspeed/ds3_llama.json
+~/miniforge3/envs/DeepKNLP/bin/python -m accelerate.commands.launch --config_file configs/accelerate/gpu4b-ds1.yaml task2-nerG-trainer2.py --accelerate_deepspeed
+~/miniforge3/envs/DeepKNLP/bin/python -m accelerate.commands.launch --config_file configs/accelerate/gpu4b-ds2.yaml task2-nerG-trainer2.py --accelerate_deepspeed
+~/miniforge3/envs/DeepKNLP/bin/python -m accelerate.commands.launch --config_file configs/accelerate/gpu4b-ds3.yaml task2-nerG-trainer2.py --accelerate_deepspeed
+~/miniforge3/envs/DeepKNLP/bin/python -m accelerate.commands.launch --config_file configs/accelerate/gpu4b.yaml task2-nerG-trainer2.py
