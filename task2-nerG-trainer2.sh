24f13dba7a02aac2c6cba53862800ef060fe8d37
Jihee Ryu <chrisjihee@etri.re.kr>
2025-01-21 19:52:38 +0900

    Update main(): deepspeed -> deepspeed_for_accel, deepspeed_for_train

--- task2-nerG-trainer2.sh
+++ task2-nerG-trainer2.sh
@@ -1,6 +1,6 @@
 set -x
-~/miniforge3/envs/DeepKNLP/bin/python -m deepspeed.launcher.runner --include=localhost:0,1,2,3 --master_port 30000 task2-nerG-trainer2.py
-~/miniforge3/envs/DeepKNLP/bin/python -m accelerate.commands.launch --config_file configs/accelerate/gpu4a.yaml task2-nerG-trainer2.py
-~/miniforge3/envs/DeepKNLP/bin/python -m accelerate.commands.launch --config_file configs/accelerate/gpu4a-ds1.yaml task2-nerG-trainer2.py --deepspeed
-~/miniforge3/envs/DeepKNLP/bin/python -m accelerate.commands.launch --config_file configs/accelerate/gpu4a-ds2.yaml task2-nerG-trainer2.py --deepspeed
-~/miniforge3/envs/DeepKNLP/bin/python -m accelerate.commands.launch --config_file configs/accelerate/gpu4a-ds2.yaml task2-nerG-trainer2.py --deepspeed
+~/miniforge3/envs/DeepKNLP/bin/python -m deepspeed.launcher.runner --include=localhost:0,1,2,3 --master_port 30000 task2-nerG-trainer2.py --deepspeed_for_train configs/deepspeed/deepspeed_zero1_llama.json
+#~/miniforge3/envs/DeepKNLP/bin/python -m accelerate.commands.launch --config_file configs/accelerate/gpu4a.yaml task2-nerG-trainer2.py
+#~/miniforge3/envs/DeepKNLP/bin/python -m accelerate.commands.launch --config_file configs/accelerate/gpu4a-ds1.yaml task2-nerG-trainer2.py --deepspeed_for_accel
+#~/miniforge3/envs/DeepKNLP/bin/python -m accelerate.commands.launch --config_file configs/accelerate/gpu4a-ds2.yaml task2-nerG-trainer2.py --deepspeed_for_accel
+~/miniforge3/envs/DeepKNLP/bin/python -m accelerate.commands.launch --config_file configs/accelerate/gpu4a-ds3.yaml task2-nerG-trainer2.py --deepspeed_for_accel
