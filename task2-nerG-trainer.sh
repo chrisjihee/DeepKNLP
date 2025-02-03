a6aada07da2e9a4dad5299a0e8505a5f9cbec8c1
Jihee Ryu <chrisjihee@etri.re.kr>
2025-02-03 17:56:53 +0900

    Update task2-nerG-trainer.sh

--- task2-nerG-trainer.sh
+++ task2-nerG-trainer.sh
@@ -5,7 +5,23 @@ port=$(shuf -i25000-30000 -n1)
 python -m deepspeed.launcher.runner --include="localhost:$CUDA_VISIBLE_DEVICES" --master_port $port \
               task2-nerG-trainer.py "data/gner/each/crossner_ai-train.jsonl" \
                         --eval_file "data/gner/each-sampled/crossner_ai-dev=100.jsonl" \
+                       --pretrained "google/flan-t5-large" \
+                      --run_version "FLAN-T5-Large" \
+      --per_device_train_batch_size 1 \
+                --trainer_deepspeed "configs/deepspeed/ds2_llama.json"
+
+python -m deepspeed.launcher.runner --include="localhost:$CUDA_VISIBLE_DEVICES" --master_port $port \
+              task2-nerG-trainer.py "data/gner/each/crossner_ai-train.jsonl" \
+                        --eval_file "data/gner/each-sampled/crossner_ai-dev=100.jsonl" \
+                       --pretrained "meta-llama/Llama-3.2-1B" \
+                      --run_version "Llama-3-1B" \
+      --per_device_train_batch_size 1 \
+                --trainer_deepspeed "configs/deepspeed/ds2_llama.json"
+
+python -m deepspeed.launcher.runner --include="localhost:$CUDA_VISIBLE_DEVICES" --master_port $port \
+              task2-nerG-trainer.py "data/gner/each/crossner_ai-train.jsonl" \
+                        --eval_file "data/gner/each-sampled/crossner_ai-dev=100.jsonl" \
                        --pretrained "etri-lirs/egpt-1.3b-preview" \
                       --run_version "EAGLE-1B-supervised" \
       --per_device_train_batch_size 1 \
-                --trainer_deepspeed "configs/deepspeed/ds1_llama.json"
+                --trainer_deepspeed "configs/deepspeed/ds2_llama.json"
