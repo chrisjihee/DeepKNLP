ef5ed9ec18209aa5ff79deb5ddfd7ed7d502d4b0
Jihee Ryu <chrisjihee@etri.re.kr>
2025-02-03 15:57:25 +0900

    Update summarize(): accept input_dirs instead of input_dir

--- task2-nerG-summarizer.py
+++ task2-nerG-summarizer.py
@@ -6,7 +6,7 @@ from typing_extensions import Annotated
 
 from chrisbase.data import AppTyper, JobTimer, NewProjectEnv
 from chrisbase.data import FileOption, FileStreamer
-from chrisbase.io import files
+from chrisbase.io import files, dirs
 from chrisbase.io import new_path
 
 # Global settings
@@ -16,34 +16,37 @@ logger: logging.Logger = logging.getLogger("DeepKNLP")
 
 @main.command("summarize")
 def summarize(
-        # for CustomDataArguments
-        input_dir: Annotated[str, typer.Argument()] = ...,  # "output/GNER-Baseline/FLAN-T5-Large-Baseline",
-        output_file: Annotated[str, typer.Option("--output_file")] = "",
+        input_dirs: Annotated[str, typer.Argument()] = ...,  # "output/GNER-Baseline/*",
+        csv_filename: Annotated[str, typer.Option("--csv_filename")] = "train-metrics-*.csv",
         logging_level: Annotated[int, typer.Option("--logging_level")] = logging.INFO,
 ):
     env = NewProjectEnv(logging_level=logging_level)
-    if not output_file:
-        output_file = new_path(input_dir, post="eval").with_suffix(".csv")
     with (
         JobTimer(f"python {env.current_file} {' '.join(env.command_args)}", rt=1, rb=1, rc='=', verbose=logging_level <= logging.INFO),
-        FileStreamer(FileOption.from_path(path=output_file, mode="w")) as output_file,
-        FileStreamer(FileOption.from_path(path=input_dir)) as input_dir,
     ):
         no_interest_columns = ['eval_average', 'eval_runtime', 'eval_samples_per_second', 'eval_steps_per_second']
-        dfs = []
-        for input_file in files(input_dir.path / "train-metrics-*.csv"):
-            df = pd.read_csv(input_file)
-            eval_columns = [col for col in df.columns if col.startswith("eval_") and col not in no_interest_columns]
-            df = df.dropna(subset=eval_columns, how="all")[["epoch"] + eval_columns]
-            df["epoch"] = df["epoch"].round(1)
-            dfs.append(df)
-        merged_df = pd.concat(dfs, ignore_index=True)
-        merged_df = merged_df.groupby("epoch").first().reset_index()
-        eval_columns = [col for col in merged_df.columns if col.startswith("eval_")]
-        merged_df["eval_average"] = merged_df[eval_columns].mean(axis=1)
-        merged_df = merged_df[["epoch", "eval_average"] + eval_columns]
-        print(merged_df)
-        merged_df.to_csv(output_file.path, index=False)
+        for input_dir in dirs(input_dirs):
+            output_file = input_dir.with_name(input_dir.name + "-eval.csv")
+            logger.info("[input_dir] %s", input_dir)
+            dfs = []
+            for input_file in files(input_dir / csv_filename):
+                df = pd.read_csv(input_file)
+                eval_columns = [col for col in df.columns if col.startswith("eval_") and col not in no_interest_columns]
+                df = df.dropna(subset=eval_columns, how="all")[["epoch"] + eval_columns]
+                if df.shape[0] == 0:
+                    continue
+                df["epoch"] = df["epoch"].round(1)
+                dfs.append(df)
+            if dfs:
+                merged_df = pd.concat(dfs, ignore_index=True)
+                merged_df = merged_df.groupby("epoch").first().reset_index()
+                eval_columns = [col for col in merged_df.columns if col.startswith("eval_")]
+                merged_df["eval_average"] = merged_df[eval_columns].mean(axis=1)
+                merged_df = merged_df[["epoch", "eval_average"] + eval_columns]
+                merged_df.to_csv(output_file, index=False)
+                logger.info(f">> {len(dfs)} files are merged into {output_file}")
+            else:
+                logger.info(f">> No files or no evaluation output in input folder")
 
 
 if __name__ == "__main__":
