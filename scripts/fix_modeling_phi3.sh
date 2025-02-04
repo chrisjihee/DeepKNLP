666dbc22948059994989fc83cdfec79462817a55
Jihee Ryu <chrisjihee@etri.re.kr>
2025-02-04 23:02:14 +0900

    Update fix_modeling_phi3.sh

--- scripts/fix_modeling_phi3.sh
+++ scripts/fix_modeling_phi3.sh
@@ -1,1 +1,2 @@
-sed -i 's/get_max_length()/get_max_cache_shape()/g' .cache_hf/hub/models--microsoft--Phi-*/snapshots/*/modeling_phi3.py
+sed -i 's/get_max_length/get_max_cache_shape/g' .cache_hf/hub/models--microsoft--Phi-*/snapshots/*/modeling_phi3.py
+cat .cache_hf/hub/models--microsoft--Phi-*/snapshots/*/modeling_phi3.py | grep get_max_
