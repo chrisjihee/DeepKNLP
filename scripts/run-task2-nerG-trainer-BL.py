0e4030a29cf3b49780d7123d3c1f4696036c1304
Jihee Ryu <chrisjihee@etri.re.kr>
2025-02-05 00:17:25 +0900

    Remove google/gemma-2-9b

--- scripts/run-task2-nerG-trainer-BL.py
+++ scripts/run-task2-nerG-trainer-BL.py
@@ -49,10 +49,9 @@ models_4B_or_less = [
 ]
 models_7B_or_more = [
     # ("configs/deepspeed/ds2_llama.json", "Llama3-8B", "meta-llama/Llama-3.1-8B"),
-    ("configs/deepspeed/ds2_llama.json", "Gemma2-9B", "google/gemma-2-9b"),
-    # ("configs/deepspeed/ds2_llama.json", "Ministral-8B", "mistralai/Ministral-8B-Instruct-2410"),
+    ("configs/deepspeed/ds2_llama.json", "Ministral-8B", "mistralai/Ministral-8B-Instruct-2410"),
     # ("configs/deepspeed/ds2_llama.json", "Qwen2-7B", "Qwen/Qwen2.5-7B"),
-    # ("configs/deepspeed/ds2_llama.json", "Phi3-7B", "microsoft/Phi-3-small-8k-instruct"),
+    # ("configs/deepspeed/ds2_llama.json", "Phi3-7B", "microsoft/Phi-3-small-8k-instruct"),  # modeling_phi3.py: get_max_length -> get_max_cache_shape
     # ("configs/deepspeed/ds2_llama.json", "Gemma-7B", "google/gemma-7b"),
     # ("configs/deepspeed/ds2_llama.json", "Mistral-7B", "mistralai/Mistral-7B-Instruct-v0.3"),
 
@@ -62,7 +61,7 @@ models_7B_or_more = [
     # ("configs/deepspeed/ds3_t5.json", "FlanT5-11B", "google/flan-t5-xxl"),
     # ("configs/deepspeed/ds3_llama.json", "Llama3-11B", "meta-llama/Llama-3.2-11B-Vision"),
     # ("configs/deepspeed/ds3_llama.json", "Mistral-12B", "mistralai/Mistral-Nemo-Instruct-2407"),
-    # ("configs/deepspeed/ds3_llama.json", "Phi3-14B", "microsoft/phi-4"),
+    # ("configs/deepspeed/ds3_llama.json", "Phi4-14B", "microsoft/phi-4"),
     # ("configs/deepspeed/ds3_llama.json", "Qwen2-14B", "Qwen/Qwen2.5-14B"),
     # ("configs/deepspeed/ds3_llama.json", "DeepSeek2-Lite-16B", "deepseek-ai/DeepSeek-V2-Lite"),
     # ("configs/deepspeed/ds3_llama.json", "Mistral-22B", "mistralai/Mistral-Small-Instruct-2409"),
