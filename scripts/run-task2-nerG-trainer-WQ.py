774adfa4453689600d8b800ee349b965cdec4b6e
Jihee Ryu <chrisjihee@etri.re.kr>
2025-02-06 23:36:07 +0900

    Updating run-task2-nerG-trainer-WQ.py

--- scripts/run-task2-nerG-trainer-WQ.py
+++ scripts/run-task2-nerG-trainer-WQ.py
@@ -9,9 +9,7 @@ hostname = socket.gethostname()
 cuda_devices = os.getenv("CUDA_VISIBLE_DEVICES", "0,1,2,3")
 large_machines = ["lirs-b1", "dl026"]
 
-# Training parameters
-run_suffix = "-WQ"
-file_suffix = "-WQ"
+# Training arguments
 eval_epochs = 0.5
 train_batch = 8
 train_epochs = 12
@@ -19,24 +17,7 @@ logging_steps = 5
 generation_max_length = 640
 small_grad_steps = 1
 large_grad_steps = 4
-eval_dir = f"data/gner/each-sampled{file_suffix}"
-train_dir = f"data/gner/each{file_suffix}"
 debugging = False
-
-# List of datasets
-datasets = [
-    "crossner_ai",
-    "crossner_music",
-    "crossner_science",
-    "crossner_politics",
-    "crossner_literature",
-    "mit-movie",
-    "mit-restaurant",
-]
-large_datasets = [
-    "mit-movie",
-    "mit-restaurant",
-]
 
 # List of pretrained models
 models_4B_or_less = [
@@ -73,39 +54,70 @@ if hostname not in large_machines:
 else:
     models = models_7B_or_more
 
+# List of datasets
+datasets = [
+    "crossner_ai",
+    "crossner_music",
+    "crossner_science",
+    "crossner_politics",
+    "crossner_literature",
+    "mit-movie",
+    "mit-restaurant",
+]
+large_datasets = [
+    "mit-movie",
+    "mit-restaurant",
+]
+
+# Suffix and datafile directories
+label_levels = [
+    "1",
+    "2",
+    "3",
+    "4,1",
+    "4,2",
+    "4,3",
+    "5",
+]
+
 # Loop through each model and dataset
 for ds_config, run_prefix, pretrained in models:
     for dataset in datasets:
-        run_version = f"{run_prefix}{run_suffix}"
-        grad_steps = large_grad_steps if dataset in large_datasets else small_grad_steps
-        use_flash_attention = pretrained.startswith("microsoft/Phi")
+        for label_level in label_levels:
+            suffix = f"-WQ={label_level}"
+            eval_dir = f"data/gner/each-sampled{suffix}"
+            train_dir = f"data/gner/each{suffix}"
+            run_version = f"{run_prefix}{suffix}"
+            grad_steps = large_grad_steps if dataset in large_datasets else small_grad_steps
+            use_flash_attention = pretrained.startswith("microsoft/Phi")
 
-        command = f"""
-            python -m
-                deepspeed.launcher.runner
-                    --include=localhost:{cuda_devices}
-                    --master_port={port}
-                task2-nerG-trainer.py
-                    --trainer_deepspeed {ds_config}
-                    --run_version {run_version}
-                    --pretrained {pretrained}
-                    --eval_epochs {eval_epochs}
-                    --logging_steps {logging_steps}
-                    --num_train_epochs {train_epochs}
-                    --per_device_train_batch_size {train_batch}
-                    --gradient_accumulation_steps {grad_steps}
-                    --generation_max_length {generation_max_length}
-                    --eval_file {eval_dir}/{dataset}-dev=100{file_suffix}.jsonl
-                    --train_file {train_dir}/{dataset}-train{file_suffix}.jsonl
-                    --output_file train-metrics-{dataset}-{train_epochs}ep.csv
-                    --logging_file train-loggings-{dataset}-{train_epochs}ep.out
-                    --{'' if use_flash_attention else 'no_'}use_flash_attention
-                    --{'' if debugging else 'no_'}debugging
-        """
-        command = command.strip().split()
-        print("*" * 120)
-        print("[COMMAND]", " ".join(command))
-        print("*" * 120)
+            command = f"""
+                python -m
+                    deepspeed.launcher.runner
+                        --include=localhost:{cuda_devices}
+                        --master_port={port}
+                    task2-nerG-trainer.py
+                        --trainer_deepspeed {ds_config}
+                        --run_version {run_version}
+                        --pretrained {pretrained}
+                        --eval_epochs {eval_epochs}
+                        --logging_steps {logging_steps}
+                        --num_train_epochs {train_epochs}
+                        --per_device_train_batch_size {train_batch}
+                        --gradient_accumulation_steps {grad_steps}
+                        --generation_max_length {generation_max_length}
+                        --eval_file {eval_dir}/{dataset}-dev=100{suffix}.jsonl
+                        --train_file {train_dir}/{dataset}-train{suffix}.jsonl
+                        --output_file train-metrics-{dataset}-{train_epochs}ep.csv
+                        --logging_file train-loggings-{dataset}-{train_epochs}ep.out
+                        --{'' if use_flash_attention else 'no_'}use_flash_attention
+                        --{'' if debugging else 'no_'}debugging
+            """
+            command = command.strip().split()
+            print("*" * 120)
+            print("[COMMAND]", " ".join(command))
+            print("*" * 120)
 
-        subprocess.run(command)
-        print("\n" * 3)
+            # subprocess.run(command)
+            # print("\n" * 3)
+        exit(0)
