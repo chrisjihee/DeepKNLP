b1749cdcb9522b1f3c85f662bb54244f719b9271
Jihee Ryu <chrisjihee@etri.re.kr>
2025-08-26 21:39:56 +0900

    Save eval metrics

--- task3-qa/train_qa-1.sh
+++ task3-qa/train_qa-1.sh
@@ -9,8 +9,19 @@ CUDA_VISIBLE_DEVICES=7 python task3-qa/train_qa.py \
   --save_total_limit 1 \
   --save_strategy epoch \
   --eval_strategy epoch \
+  --logging_strategy steps \
+  --logging_steps 10 \
   --per_device_train_batch_size 50 \
   --max_seq_length 384 \
   --doc_stride 128 \
   --learning_rate 3e-5 \
   --overwrite_output_dir
+
+# ***** eval metrics *****
+#   epoch                   =        1.0
+#   eval_exact_match        =       86.8
+#   eval_f1                 =    91.4781
+#   eval_runtime            = 0:00:18.46
+#   eval_samples            =       3466
+#   eval_samples_per_second =    187.685
+#   eval_steps_per_second   =     23.501
--- task3-qa/train_qa-2.sh
+++ task3-qa/train_qa-2.sh
@@ -9,8 +9,19 @@ CUDA_VISIBLE_DEVICES=6 python task3-qa/train_qa.py \
   --save_total_limit 1 \
   --save_strategy epoch \
   --eval_strategy epoch \
+  --logging_strategy steps \
+  --logging_steps 10 \
   --per_device_train_batch_size 50 \
   --max_seq_length 384 \
   --doc_stride 128 \
   --learning_rate 3e-5 \
   --overwrite_output_dir
+
+# ***** eval metrics *****
+#   epoch                   =        1.0
+#   eval_exact_match        =    83.9333
+#   eval_f1                 =    89.2887
+#   eval_runtime            = 0:00:18.47
+#   eval_samples            =       3464
+#   eval_samples_per_second =    187.537
+#   eval_steps_per_second   =     23.442
