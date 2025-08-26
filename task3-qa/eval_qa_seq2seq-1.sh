8ec6476e2e337015c7bb1aa3237de7b835009ce7
Jihee Ryu <chrisjihee@etri.re.kr>
2025-08-27 07:08:39 +0900

    Update eval_qa_seq2seq-*.sh

--- task3-qa/eval_qa_seq2seq-1.sh
+++ task3-qa/eval_qa_seq2seq-1.sh
@@ -1,13 +1,21 @@
-CUDA_VISIBLE_DEVICES=4 python task3-qa/train_qa_seq2seq.py \
-  --train_file data/korquad/train.jsonl \
+OUTPUT_DIR=output/korquad/train_qa_by-pkot5-at-dev0-test
+CHECKPOINT_DIR=output/korquad/train_qa_by-pkot5-at-dev0/checkpoint-2500
+CUDA_VISIBLE_DEVICES=0 python task3-qa/train_qa_seq2seq.py \
+  --train_file data/korquad/train-half.jsonl \
   --validation_file data/korquad/validation.jsonl \
-  --output_dir output/korquad/train_qa_seq2seq-by-ket5 \
-  --model_name_or_path output/korquad/train_qa_seq2seq-by-ket5/checkpoint-10068 \
-  --predict_with_generate \
+  --output_dir $OUTPUT_DIR \
+  --overwrite_output_dir \
+  --model_name_or_path $CHECKPOINT_DIR \
   --do_eval \
-  --per_device_eval_batch_size 16 \
-  --overwrite_output_dir
+  --bf16 \
+  --per_device_eval_batch_size 8 \
+  --max_seq_length 512 \
+  --predict_with_generate
 
+OUTPUT_DIR=output/korquad/train_qa_by-pkot5-at-dev0-test
 python task3-qa/evaluate-KorQuAD-v1.py \
        data/korquad/KorQuAD_v1.0_dev.json \
-       output/korquad/train_qa_seq2seq-by-ket5/eval_predictions.json
+       $OUTPUT_DIR/eval_predictions.json
+
+# pko-t5-base, lr=5e-5: {"exact_match": 71.56217526844475, "f1": 79.15031011581894}
+# pko-t5-large, lr=5e-5: {"exact_match": 74.07343262902667, "f1": 80.87612476775051}
