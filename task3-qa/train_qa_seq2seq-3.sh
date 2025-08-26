088cf46f903e80a56a11ae15ee9da9268fd31611
Jihee Ryu <chrisjihee@etri.re.kr>
2025-08-27 05:21:54 +0900

    Updating train_qa_seq2seq-*.sh

--- task3-qa/train_qa_seq2seq-3.sh
+++ task3-qa/train_qa_seq2seq-3.sh
@@ -2,7 +2,7 @@ OUTPUT_DIR=output/korquad/train_qa_by-pkot5-at-dev2
 CUDA_VISIBLE_DEVICES=2 python task3-qa/train_qa_seq2seq.py \
   --train_file data/korquad/train-half.jsonl \
   --validation_file data/korquad/validation.jsonl \
-  --model_name_or_path paust/pko-t5-base \
+  --model_name_or_path paust/pko-chat-t5-large \
   --output_dir $OUTPUT_DIR \
   --overwrite_output_dir \
   --do_train \
@@ -14,7 +14,7 @@ CUDA_VISIBLE_DEVICES=2 python task3-qa/train_qa_seq2seq.py \
   --eval_strategy epoch \
   --logging_strategy steps \
   --logging_steps 10 \
-  --per_device_train_batch_size 16 \
+  --per_device_train_batch_size 12 \
   --gradient_accumulation_steps 1 \
   --max_seq_length 512 \
   --learning_rate 5e-5 \
@@ -26,7 +26,5 @@ python task3-qa/evaluate-KorQuAD-v1.py \
 
 # 2025.02 (large): {"exact_match": 74.78351229650156, "f1": 81.67965558409172}
 
-# 2025.08.27 (bf16, lr=3e-5, seq=384): {"exact_match": 48.26809837201247, "f1": 55.787263003319154}
-# 2025.08.27 (bf16, lr=3e-5): {"exact_match": 70.31520609629374, "f1": 78.15017267878011}
-# 2025.08.27 (fp16, lr=3e-5): {"exact_match": 70.15933494977486, "f1": 78.20142143881837}
-# 2025.08.27 (bf16, lr=5e-5): {"exact_match": 71.56217526844475, "f1": 79.15031011581894}
+# 2025.08.27 (base, lr=3e-5): {"exact_match": 70.31520609629374, "f1": 78.15017267878011}
+# 2025.08.27 (base, lr=5e-5): {"exact_match": 71.56217526844475, "f1": 79.15031011581894}
