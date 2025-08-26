d679c56f7c2d7c22c7657439aa464840baf5db27
Jihee Ryu <chrisjihee@etri.re.kr>
2025-08-27 03:45:51 +0900

    Update train_qa-*.sh

--- task3-qa/train_qa-1.sh
+++ task3-qa/train_qa-1.sh
@@ -1,38 +1,45 @@
+OUTPUT_DIR=output/korquad/train_qa-by-kpfbert
 CUDA_VISIBLE_DEVICES=7 python task3-qa/train_qa.py \
   --train_file data/korquad/train-half.jsonl \
   --validation_file data/korquad/validation.jsonl \
   --model_name_or_path jinmang2/kpfbert \
-  --output_dir output/korquad/train_qa-by-kpfbert \
+  --output_dir $OUTPUT_DIR \
   --overwrite_output_dir \
   --do_train \
   --do_eval \
-  --fp16 \
+  --bf16 \
   --num_train_epochs 1 \
   --save_total_limit 1 \
   --save_strategy epoch \
   --eval_strategy epoch \
   --logging_strategy steps \
   --logging_steps 10 \
-  --per_device_train_batch_size 32 \
+  --per_device_train_batch_size 12 \
   --gradient_accumulation_steps 1 \
   --max_seq_length 512 \
   --learning_rate 3e-5 \
   --doc_stride 128
 
+python task3-qa/evaluate-KorQuAD-v1.py \
+       data/korquad/KorQuAD_v1.0_dev.json \
+       $OUTPUT_DIR/eval_predictions.json
+
 #***** train metrics *****
 #  epoch                    =        1.0
 #  total_flos               =  7575290GF
-#  train_loss               =     0.6886
-#  train_runtime            = 0:03:06.75
+#  train_loss               =     0.5903
+#  train_runtime            = 0:03:30.73
 #  train_samples            =      31129
-#  train_samples_per_second =     166.68
-#  train_steps_per_second   =       5.21
+#  train_samples_per_second =    147.714
+#  train_steps_per_second   =     12.314
 
 #***** eval metrics *****
 #  epoch                   =        1.0
-#  eval_exact_match        =    85.8504
-#  eval_f1                 =     91.178
-#  eval_runtime            = 0:00:12.46
+#  eval_exact_match        =    86.1275
+#  eval_f1                 =    91.3063
+#  eval_runtime            = 0:00:12.39
 #  eval_samples            =       6152
-#  eval_samples_per_second =    493.472
-#  eval_steps_per_second   =     61.684
+#  eval_samples_per_second =    496.474
+#  eval_steps_per_second   =     62.059
+
+#{"exact_match": 86.26602009005889, "f1": 94.36013888428921}
--- task3-qa/train_qa-3.sh
+++ task3-qa/train_qa-3.sh
@@ -1,38 +1,45 @@
+OUTPUT_DIR=output/korquad/train_qa-by-koelectra
 CUDA_VISIBLE_DEVICES=5 python task3-qa/train_qa.py \
   --train_file data/korquad/train-half.jsonl \
   --validation_file data/korquad/validation.jsonl \
   --model_name_or_path monologg/koelectra-base-v3-discriminator \
-  --output_dir output/korquad/train_qa-by-koelectra \
+  --output_dir $OUTPUT_DIR \
   --overwrite_output_dir \
   --do_train \
   --do_eval \
-  --fp16 \
+  --bf16 \
   --num_train_epochs 1 \
   --save_total_limit 1 \
   --save_strategy epoch \
   --eval_strategy epoch \
   --logging_strategy steps \
   --logging_steps 10 \
-  --per_device_train_batch_size 32 \
+  --per_device_train_batch_size 12 \
   --gradient_accumulation_steps 1 \
   --max_seq_length 512 \
   --learning_rate 3e-5 \
   --doc_stride 128
 
+python task3-qa/evaluate-KorQuAD-v1.py \
+       data/korquad/KorQuAD_v1.0_dev.json \
+       $OUTPUT_DIR/eval_predictions.json
+
 #***** train metrics *****
 #  epoch                    =        1.0
 #  total_flos               =  7588675GF
-#  train_loss               =     1.0164
-#  train_runtime            = 0:04:15.29
+#  train_loss               =     0.8114
+#  train_runtime            = 0:04:47.43
 #  train_samples            =      31184
-#  train_samples_per_second =     122.15
-#  train_steps_per_second   =      3.819
+#  train_samples_per_second =    108.491
+#  train_steps_per_second   =      9.042
 
 #***** eval metrics *****
 #  epoch                   =        1.0
-#  eval_exact_match        =    82.4558
-#  eval_f1                 =    88.5156
-#  eval_runtime            = 0:00:17.78
+#  eval_exact_match        =    84.0665
+#  eval_f1                 =    89.5272
+#  eval_runtime            = 0:00:17.89
 #  eval_samples            =       6178
-#  eval_samples_per_second =    347.297
-#  eval_steps_per_second   =     43.454
+#  eval_samples_per_second =    345.274
+#  eval_steps_per_second   =     43.201
+
+#{"exact_match": 84.20505715275372, "f1": 92.90331398272818}
